4a5ce337c8f02d4818c0a54cf5d78830743af94b
Luke Harrison <luketheduke.harrison@gmail.com>
2022-12-05 10:27:08 +0000

    Day5 2022

--- 2022/day5.py
+++ 2022/day5.py
@@ -4,9 +4,26 @@ input = AocBot.data
 
 # Part 1
 def p1(submit=False):
-    total = 0
+    initial = {
+        1:[x for x in 'BSVZGPW'],
+        2:[x for x in 'JVBCZF'],
+        3:[x for x in 'VLMHNZDC'],
+        4:[x for x in 'LDMZPFJB'],
+        5:[x for x in 'VFCGJBQH'],
+        6:[x for x in 'GFQTSLB'],
+        7:[x for x in 'LGCZV'],
+        8:[x for x in 'NLG'],
+        9:[x for x in 'JFHC']
+    }
+    total = ""
     for line in input:
-        ...
+        count,fp,sp = line.replace('move ','').replace(' from ',':').replace(' to ',':').split(':')
+        count,fp,sp = int(count),int(fp),int(sp)
+        for i in range(count):
+            initial[sp].append(initial[fp].pop())
+
+    for i in range(1,10):
+        total += initial[i][-1]
 
     if submit: AocBot.submit('1', total)
     else: print(total)
@@ -14,9 +31,28 @@ def p1(submit=False):
 
 # Part 2
 def p2(submit=False):
-    total = 0
+    initial = {
+        1:[x for x in 'BSVZGPW'],
+        2:[x for x in 'JVBCZF'],
+        3:[x for x in 'VLMHNZDC'],
+        4:[x for x in 'LDMZPFJB'],
+        5:[x for x in 'VFCGJBQH'],
+        6:[x for x in 'GFQTSLB'],
+        7:[x for x in 'LGCZV'],
+        8:[x for x in 'NLG'],
+        9:[x for x in 'JFHC']
+    }
+    total = ""
     for line in input:
-        ...
+        count,fp,sp = line.replace('move ','').replace(' from ',':').replace(' to ',':').split(':')
+        count,fp,sp = int(count),int(fp),int(sp)
+        for i in initial[fp][-count:]:
+            initial[sp].append(i)
+        for i in range(count):
+            initial[fp].pop()
+
+    for i in range(1,10):
+        total += initial[i][-1]
 
     if submit: AocBot.submit('2', total)
     else: print(total)
